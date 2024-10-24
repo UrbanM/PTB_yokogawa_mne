23b28df2f92054e449e2c93b48c3e9d76822b99f
UrbanM <urban.marhl@imfm.si>
2024-10-24 11:43:05 +0200

    Initital commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PTB_yokogawa_mne
 This is an example project, how to convert the yokogawa SQUID measurements at PTB to the MNE .fif format with geometry..
+
+To run the main.py script you need to install: mne, pyvistaqt, traitlets
---
+++ main.py
@@ -0,0 +1,67 @@
+# This is a sample Python script.
+# Created by Urban Marhl
+
+def import_coregistration2(zebris_file):
+    import re
+    import numpy as np
+
+    elp_zebris = np.zeros((8, 3))
+
+    zeb = []
+    F = open(zebris_file, 'r')
+    num_lines = sum(1 for line in open(zebris_file))
+    i = 0
+    while i < num_lines:
+        f = F.readline()
+        f = f.replace('"', '')
+        f = f.replace(':', ',')
+        f = f.replace('\n', '')
+        f = f.replace('\t\t', ' ')
+        f = f.replace('\t', '')
+        ff = list(filter(None, re.split(" ", f)))
+        ffd = np.array(ff[1:4], dtype=float)
+        if (ff[0] == 'Coil1'):
+            elp_zebris[3] += ffd
+        elif (ff[0] == 'Coil2'):
+            elp_zebris[4] += ffd
+        elif (ff[0] == 'Coil3'):
+            elp_zebris[5] += ffd
+        elif (ff[0] == 'Coil4'):
+            elp_zebris[6] += ffd
+        elif (ff[0] == 'Coil5'):
+            elp_zebris[7] += ffd
+        elif (ff[0] == 'Na'):
+            elp_zebris[0] += ffd
+        elif (ff[0] == 'LP'):
+            elp_zebris[1] += ffd
+        elif (ff[0] == 'RP'):
+            elp_zebris[2] += ffd
+        elif (i > 2):
+            zeb.append(ff[1:4])
+        i += 1
+
+    zeb = np.array(zeb, dtype=float)
+    elp_zebris[:, :] /= 2
+
+    return elp_zebris * 10 ** (-3), zeb * 10 ** (-3)
+
+
+def preprocess():
+    import mne
+
+    con_name = "DATA/urma181019.0300.con"
+    zebris_file = "DATA/urma181019.0300.sfp"
+    marker_file = "DATA/urma181019.0300.mrk"
+    subject_dir = "DATA/subjects/"
+
+    elp, hsp = import_coregistration2(zebris_file)
+
+    raw = mne.io.read_raw_kit(con_name, mrk=marker_file, elp=elp, hsp=hsp)
+
+    mne.gui.coregistration(subjects_dir=subject_dir, subject="urma")
+
+    return
+
+
+if __name__ == '__main__':
+    preprocess()
